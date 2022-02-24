e3569023ffe7f34491b543ec72c515ddf1f2eb1a
Alessandra Soares dos Santos <AlessandraSoaresdosSantos@users.noreply.github.com>
2022-02-24 20:42:32 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoExemploA;
+using ProjetoExemploB;
+using System;
+
+namespace ExemploConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkItem item = new WorkItem("Exemplo de Modificador Acesso",
+                            "Exemplo modificador acesso da classe WorkItem");
+
+           
+            ChangeRequest change = new ChangeRequest
+                ("Modificador Acesso - ChangeRequest",
+                 "Adicionando informações no modificador acesso",
+                 1);
+
+            
+            Console.WriteLine(item.ReturnWorkItemInformation(item));
+
+          
+            change.Update("Título alterado");
+
+            
+            Console.WriteLine(change.ReturnChangeRequestInformation(change));
+        }
+    }
+}
---
+++ ProjetoExemploB/ChangeRequest.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoExemploA;
+
+namespace ProjetoExemploB
+{
+    public class ChangeRequest : WorkItem
+    {
+        protected int OriginalItemID { get; set; }
+ 
+        public ChangeRequest() { }
+
+        public ChangeRequest(string title, string desc, int originalID)
+        {
+            
+            this.ID = GetNextID();
+            this.Title = title;
+            this.Description = desc;
+           
+           
+            this.OriginalItemID = originalID;
+        }
+
+        public  string ReturnChangeRequestInformation(ChangeRequest changeRequest)
+        {
+            return $"ID: {changeRequest.ID} - Title: {changeRequest.Title} - Description:{changeRequest.Description}";
+        }
+
+    }
+}
